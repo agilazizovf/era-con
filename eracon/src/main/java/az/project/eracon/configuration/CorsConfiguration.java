1f51e7a88990b21986641b78dfd352483634f59e
AqilAzizov2005 <aqilazizov2005@gmail.com>
2025-06-30 00:18:42 +0400

    nese

--- eracon/src/main/java/az/project/eracon/configuration/CorsConfiguration.java
+++ eracon/src/main/java/az/project/eracon/configuration/CorsConfiguration.java
@@ -1,20 +1,19 @@
 package az.project.eracon.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration {
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Bütün endpointlər üçün
-                        .allowedOrigins("*") // İstənilən mənşə üçün
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // İcazə verilən metodlar
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173") // Vite dev server
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
