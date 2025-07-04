79ef0b2fe99d576544dcf7cb609f284fa85281a5
AqilAzizov2005 <aqilazizov2005@gmail.com>
2025-07-04 14:00:37 +0400

    push

--- eracon/src/main/java/az/project/eracon/controller/MediaHeaderPictureController.java
+++ eracon/src/main/java/az/project/eracon/controller/MediaHeaderPictureController.java
@@ -2,8 +2,12 @@ package az.project.eracon.controller;
 
 import az.project.eracon.service.MediaHeaderPictureService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @MyRestController
 @RequestMapping("/api/media-header-picture")
@@ -12,10 +16,11 @@ public class MediaHeaderPictureController {
 
     private final MediaHeaderPictureService service;
 
-    @PostMapping("/upload")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public void uploadPicture(@RequestParam("pictureUrl") String pictureUrl) {
-        service.uploadPicture(pictureUrl);
+    @PostMapping("/upload")
+    public ResponseEntity<Void> uploadPicture(@RequestParam("picture") MultipartFile picture) throws IOException {
+        service.uploadPicture(picture);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
--- eracon/src/main/java/az/project/eracon/controller/ServiceHeaderPictureController.java
+++ eracon/src/main/java/az/project/eracon/controller/ServiceHeaderPictureController.java
@@ -2,8 +2,12 @@ package az.project.eracon.controller;
 
 import az.project.eracon.service.ServiceHeaderPictureService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @MyRestController
 @RequestMapping("/api/service-header-picture")
@@ -12,10 +16,11 @@ public class ServiceHeaderPictureController {
 
     private final ServiceHeaderPictureService service;
 
-    @PostMapping("/upload")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public void uploadPicture(@RequestParam("pictureUrl") String pictureUrl) {
-        service.uploadPicture(pictureUrl);
+    @PostMapping("/upload")
+    public ResponseEntity<Void> uploadPicture(@RequestParam("picture") MultipartFile picture) throws IOException {
+        service.uploadPicture(picture);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
