eba7a1ea418753e085b189acf5dc721af15c76c2
AqilAzizov2005 <153845773+agilazizovf@users.noreply.github.com>
2025-07-06 10:15:34 +0400

    servlet

--- eracon/src/main/java/az/project/eracon/EraconApplication.java
+++ eracon/src/main/java/az/project/eracon/EraconApplication.java
@@ -2,12 +2,18 @@ package az.project.eracon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class EraconApplication {
 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(EraconApplication.class);
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EraconApplication.class, args);
 	}
-
 }
